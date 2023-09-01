c3af189ca7f6edf284fce5a7de43ba562532cadc
Dave Speer <dave.speer@coxautoinc.com>
2023-09-01 17:57:59 -0500

    wip

--- stencil/src/components/tide-chart/tide-chart.tsx
+++ stencil/src/components/tide-chart/tide-chart.tsx
@@ -1,5 +1,6 @@
 import { Component, h, Host } from "@stencil/core";
 import { GetTides, TideResponse } from '../../services/TideApi';
+import { State } from "@stencil/core";
 
 @Component({
 	tag: 'ds-tide-chart',
@@ -7,22 +8,53 @@ import { GetTides, TideResponse } from '../../services/TideApi';
 	shadow: true
 })
 export class TideChart {
+	@State() beginDate : string;
+	@State() endDate : string;
 
-	_getTides(): TideResponse[] {
-		let response = GetTides();
-		console.log('getTides response:', response);
-		return response;
+
+	@State() tides : TideResponse[] = [];
+	@State() tidesMinY: number;
+	@State() tidesMaxY: number;
+
+	_getTides() {
+		this.tides = GetTides();
+		console.log('getTides response:', this.tides);
+		this._getYAxisRange();
+	}
+
+	_getYAxisRange() {
+		this.tidesMinY = this.tides.reduce((min, current) => current.level < min ? current.level : min, this.tides[0].level);
+		this.tidesMaxY = this.tides.reduce((max, current) => current.level > max ? current.level : max, this.tides[0].level);
 	}
 
 	render() {
-		let content = <ul>{this._getTides().map(result =>
-			<li><strong>{result.when.toDateString()}</strong> - {result.level}</li>
+		let content = <ul>{this.tides.map(result =>
+			<li><strong>{result.when.toDateString()}</strong> - Ms since 1970: {result.when.getTime()} Level: {result.level}</li>
 		)}</ul>;
 
 		return (
 			<Host>
 				<h1>Tide shittle</h1>
+				<div>
+					<p>
+						Begin Date: <input
+							id="begin-date"
+							value={this.beginDate}
+						/>
+					</p>
+					<p>
+						End Date: <input
+							id="end-date"
+							value={this.endDate}
+						/>
+					</p>
+					<button onClick={this._getTides.bind(this)}>Get Tides</button>
+
+				</div>
+				<h2>debug stuff after here, would be the chart</h2>
 				{content}
+				<p>Max Y: {this.tidesMaxY}</p>
+				<p>Min Y: {this.tidesMinY}</p>
 			</Host>
 		);
 	}
