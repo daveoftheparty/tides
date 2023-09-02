14b24e1c9204f4c18105f3b9396c60f178f51963
Dave Speer <dave.speer@coxautoinc.com>
2023-09-02 10:37:33 -0500

    wip

--- stencil/src/components/tide-chart/tide-chart.tsx
+++ stencil/src/components/tide-chart/tide-chart.tsx
@@ -27,16 +27,39 @@ export class TideChart {
 		this.tidesMaxY = this.tides.reduce((max, current) => current.level > max ? current.level : max, this.tides[0].level);
 	}
 
+	_getChartDays(startDate: Date, endDate: Date): number {
+		const msInDay = 24 * 60 * 60 * 1000;
+
+		return Math.round(
+			Math.abs(Number(endDate) - Number(startDate)) / msInDay
+		) + 1;
+	}
+
 	render() {
 		let content = <ul>{this.tides.map(result =>
 			<li><strong>{result.when.toDateString()}</strong> - Ms since 1970: {result.when.getTime()} Level: {result.level}</li>
 		)}</ul>;
 
+		/****** BEGIN items that may need to transfer to state later ******/
+
+		const chartWidth = 800;
+		const chartHeight = 450;
+
+		// gonna try to do without a separate chart background, chart area, and instead, put the chart in a div and specify padding/margin
+		// const chartAreaXOffset = 40;
+		// const chartAreaYOffset = 20;
+
+		const beginDate = new Date('2023-09-06 14:27');
+		const endDate = new Date('2023-09-07 15:31');
+		const chartDays = this._getChartDays(beginDate, endDate);
+
+		console.log('chart days', chartDays);
+		/****** END items that may need to transfer to state later ******/
+
 		return (
 			<Host>
-				<h1>Tide shittle</h1>
 				<div>
-					<p>
+					{/* <p>
 						Begin Date: <input
 							id="begin-date"
 							value={this.beginDate}
@@ -47,7 +70,7 @@ export class TideChart {
 							id="end-date"
 							value={this.endDate}
 						/>
-					</p>
+					</p> */}
 					<button onClick={this._getTides.bind(this)}>Get Tides</button>
 
 				</div>
@@ -55,6 +78,11 @@ export class TideChart {
 				{content}
 				<p>Max Y: {this.tidesMaxY}</p>
 				<p>Min Y: {this.tidesMinY}</p>
+				<h2>let's rock this chart:</h2>
+
+				<svg id="chart" viewBox={`0 0 ${chartWidth} ${chartHeight}`}>
+					<rect id="chartArea" width="100%" height="100%" />
+				</svg>
 			</Host>
 		);
 	}
