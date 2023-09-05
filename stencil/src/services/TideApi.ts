af635859e00ef848782a9ba93e7263496246741e
Dave Speer <dave.speer@coxautoinc.com>
2023-09-05 14:16:18 -0500

    broken: TideApi

--- stencil/src/services/TideApi.ts
+++ stencil/src/services/TideApi.ts
@@ -15,7 +15,51 @@ export type TideResponse = {
 };
 
 export function GetTides(start: Date, end: Date): TideResponse[] {
-	return _mockTides();
+	return _getTides(start, end);
+	// return _mockTides();
+}
+
+function _yyyymmdd(input: Date) : string {
+	const mm = input.getUTCMonth() + 1; // getMonth() is zero-based
+	const dd = input.getUTCDate();
+
+	return [
+		input.getUTCFullYear(),
+		(mm > 9 ? '' : '0') + mm,
+		(dd > 9 ? '' : '0') + dd
+	].join('');
+}
+
+function _getTides(start: Date, end: Date): TideResponse[] {
+	let results: TideResponse[] = [];
+    fetch(`https://tidesandcurrents.noaa.gov/api/datagetter?product=predictions&application=NOS.COOPS.TAC.WL&begin_date=${_yyyymmdd(start)}&end_date=${_yyyymmdd(end)}&datum=MLLW&station=8775870&time_zone=gmt&units=english&interval=hilo&format=json`,
+	{mode: 'no-cors'})
+		.then(res => res.json())
+		.then(parsedRes => {
+			console.log('parsed json is ', parsedRes);
+			results = parsedRes['predictions'].map(tide => {
+				return {
+					when: _noaaStringToDate(tide.t),
+					level: +tide.v
+				};
+			});
+			console.log('tide api results', results);
+		})
+		.catch(err => {
+			console.log('we caught one');
+			console.log(err);
+		})
+		return results;
+}
+
+function _noaaStringToDate(input: string) : Date {
+	const year = +input.slice(0, 4);
+	const month = +input.slice(5, 7) - 1;
+	const day = +input.slice(8, 10);
+	const hour = +input.slice(11, 13);
+	const minute = +input.slice(14, 16);
+
+	return new Date(Date.UTC(year, month, day, hour, minute, 0, 0));
 }
 
 function _mockTides(): TideResponse[] {
