59eb5117a0004451825e5fa7891092cba370b8a5
Dave Speer <dave.speer@coxautoinc.com>
2023-09-11 10:27:10 -0500

    copypasta: pulling YTick calculations from repo blazor-combo-chart

---
+++ csharp/YAxis/YTick.cs
@@ -0,0 +1,92 @@
+namespace YAxis;
+
+public interface ISvgComboChartData
+{
+	int Key { get; }
+	double ColumnValue { get; }
+	double LineValue { get; }
+}
+
+public class SvgComboChartData : ISvgComboChartData
+{
+	public int Key { get; init; }
+	public double ColumnValue { get; init; }
+	public double LineValue { get; init; }
+}
+
+public class YTick
+{
+	private int _tickCount = 5;
+
+	public IReadOnlyList<(double Y, int label)> GetYAxisTicks(IReadOnlyList<ISvgComboChartData> data, int width, int height, bool isColumn)
+	{
+		/*
+			technically, we probably only have room for say 5 ticks. can make this
+			configurable later based on height, because very tall graphs will have
+			more room for text
+		*/
+
+		// first, calc tickHeights: the double value that will translate to svg Y coordinates
+		var tickHeights = new double[_tickCount];
+
+		// we want to have a zero base, and one tick at the top that is higher than the rest of our items
+
+		var tickHeight = height / (_tickCount - 1);
+
+		for (int i = 0; i < _tickCount; i++)
+			tickHeights[i] = i * tickHeight;
+
+		// next, calc tickLabels: the integer values that will display on svg Y axis
+		var tickLabels = new int[_tickCount];
+
+		var max = data.Max(d => isColumn ? d.ColumnValue : d.LineValue);
+		var min = data.Min(d => isColumn ? d.ColumnValue : d.LineValue);
+
+		var increments = new List<int>
+		{
+			5,
+			10,
+			15,
+			20,
+			25,
+			40,
+			50,
+			60,
+			75,
+			80,
+			100,
+			150,
+			200,
+			250,
+			300,
+			400,
+			500,
+			600,
+			700,
+			800,
+			900,
+			1000,
+			10000,
+			100000,
+			1000000,
+			10000000,
+			100000000,
+		};
+
+#warning gonna need some abs() logic here later...
+		var candidate = increments.Find(x => x >= min && (_tickCount - 1) * x >= max);
+		if(candidate == 0)
+			throw new Exception("no increment found!");
+
+
+		for (int i = 0; i < _tickCount; i++)
+			tickLabels[i] = i * candidate;
+
+		return tickHeights
+			.Zip(tickLabels)
+			.Select(x => (x.First, x.Second))
+			.ToList();
+	}
+
+
+}
---
+++ csharp/YAxis/YTickTests.cs
@@ -0,0 +1,59 @@
+namespace YAxis;
+
+public class YTickTests
+{
+	private readonly ITestOutputHelper _out;
+
+	public YTickTests(ITestOutputHelper @out)
+	{
+		_out = @out;
+	}
+
+	[Theory]
+	[InlineData(0, 100, new[] { 4, }, new[] { 0d, 25d, 50d, 75d, 100d }, new[] { 0, 5, 10, 15, 20 })] // yeah, width doesn't matter with current implementation
+	[InlineData(140, 20, new[] { 5, 10, 15 }, new[] { 0d, 5d, 10d, 15d, 20d }, new[] { 0, 5, 10, 15, 20 })]
+	[InlineData(140, 20, new[] { 3, 71, 42 }, new[] { 0d, 5d, 10d, 15d, 20d }, new[] { 0, 20, 40, 60, 80, })]
+	[InlineData(140, 20, new[] { 18, 94, 326 }, new[] { 0d, 5d, 10d, 15d, 20d }, new[] { 0, 100, 200, 300, 400 })]
+	[InlineData(140, 20, new[] { 89, 71, 117, 265, 280, }, new[] { 0d, 5d, 10d, 15d, 20d }, new[] { 0, 75, 150, 225, 300 })]
+	[InlineData(140, 20, new[] { 17, 15, 21, 32, 41, }, new[] { 0d, 5d, 10d, 15d, 20d }, new[] { 0, 15, 30, 45, 60 })]
+	public void TickTests(int width, int height, int[] data, double[] expectedTickHeights, int[] expectedTickLabels)
+	{
+		var svg = new YTick();
+
+		var input = data
+			.Select((d, index) => new SvgComboChartData
+			{
+				Key = index,
+				ColumnValue = d
+			})
+			.ToList();
+
+		var actual = svg.GetYAxisTicks(input, width, height, true).ToList();
+
+		_out.WriteLine("debugging values:");
+		foreach(var item in actual)
+		{
+			_out.WriteLine($"tick {item}");
+		}
+
+		// assert tick heights (Y coord)
+		Assert.Equal(expectedTickHeights.Length, actual.Count);
+
+		for (int i = 0; i < expectedTickHeights.Length; i++)
+		{
+			// range for double rounding issues...
+			Assert.InRange(actual[i].Y, expectedTickHeights[i] - 1, expectedTickHeights[i] + 1);
+		}
+
+		try
+		{
+			Assert.Equal(string.Join(',', expectedTickLabels), string.Join(',', actual.Select(a => a.label)));
+		}
+		catch
+		{
+			_out.WriteLine($"input for failed test: {string.Join(',', data)}");
+			throw;
+		}
+	}
+
+}
