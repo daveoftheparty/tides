d72473c74dfd45de82d48de7b359ac015ef776fa
Dave Speer <dave.speer@coxautoinc.com>
2025-09-23 09:10:09 -0500

    refactor: rename types in moon service

--- stencil/src/services/Moon.ts
+++ stencil/src/services/Moon.ts
@@ -3,26 +3,28 @@ import SunCalc from 'suncalc';
 
 
 // see moon.md in sandbox folder for more info
-export type MoonDebug = {
+
+
+export type MoonRiseSet = {
+	rise: Date,
+	set: Date
+};
+
+
+export function GetMoonTimes(start: Date, end: Date, lat: number, long: number): MoonRiseSet[] {
+	return _getMoonTimes(start, end, lat, long);
+}
+
+
+type MoonRiseRaw = {
 	when: Date,
 	rise: Date,
 	set: Date
 };
 
-export type MoonResponse = {
-	rise: Date,
-	set: Date
-};
-
-
-export function GetMoonTimes(start: Date, end: Date, lat: number, long: number): MoonResponse[] {
-	return _getMoonTimes(start, end, lat, long);
-}
-
-//https://github.com/mourner/suncalc
-function _getMoonTimes(start: Date, end: Date, lat: number, long: number) : MoonResponse[] {
-	const debug: MoonDebug[] = [];
-	const result: MoonResponse[] = [];
+function _getMoonTimes(start: Date, end: Date, lat: number, long: number) : MoonRiseSet[] {
+	const raw: MoonRiseRaw[] = [];
+	const result: MoonRiseSet[] = [];
 
 	// get moon data for a couple of days before and after the range to ensure we get moonrise/set for edge cases
 	let adjustedStart = new Date(start);
@@ -33,21 +35,21 @@ function _getMoonTimes(start: Date, end: Date, lat: number, long: number) : Moon
 	DateRangeToArray(adjustedStart, adjustedEnd).map(d => {
 		const moonrise = SunCalc.getMoonTimes(d, lat, long);
 
-		debug.push({
+		raw.push({
 			when: d,
 			rise: moonrise.rise,
 			set: moonrise.set
 		})
 	});
 
-	console.log('moon results', debug);
+	console.log('moon results', raw);
 	// debug.map(r => {
 	// 	console.log(formatDebug(r));
 	// });
 
 
 	// sort our debug data by rise time
-	debug.sort((a, b) => {
+	raw.sort((a, b) => {
 		if(!a.rise && !b.rise) return 0;
 		if(!a.rise) return 1;
 		if(!b.rise) return -1;
@@ -58,20 +60,20 @@ function _getMoonTimes(start: Date, end: Date, lat: number, long: number) : Moon
 
 	let currentRise: Date = new Date(0, 0, 0);
 
-	for(let i = 0; i < debug.length; ) {
+	for(let i = 0; i < raw.length; ) {
 		let currentSet: Date | undefined = undefined;
 
 		let riseIndex: number = i;
-		for(; riseIndex < debug.length; riseIndex++) {
-			if(debug[riseIndex].rise && debug[riseIndex].rise.valueOf() > currentRise.valueOf()) {
-				currentRise = debug[riseIndex].rise;
+		for(; riseIndex < raw.length; riseIndex++) {
+			if(raw[riseIndex].rise && raw[riseIndex].rise.valueOf() > currentRise.valueOf()) {
+				currentRise = raw[riseIndex].rise;
 				break;
 			}
 		}
 
-		for(let setIndex = riseIndex; setIndex < debug.length; setIndex++) {
-			if(debug[setIndex].set && debug[setIndex].set.valueOf() > currentRise.valueOf()) {
-				currentSet = debug[setIndex].set;
+		for(let setIndex = riseIndex; setIndex < raw.length; setIndex++) {
+			if(raw[setIndex].set && raw[setIndex].set.valueOf() > currentRise.valueOf()) {
+				currentSet = raw[setIndex].set;
 				break;
 			}
 		}
@@ -86,9 +88,9 @@ function _getMoonTimes(start: Date, end: Date, lat: number, long: number) : Moon
 	return result;
 }
 
-function formatDebug(r: MoonDebug) : string {
-	const whenLog = !r.when ? 'undefined' : ToLocalISOString(r.when).substring(0, 10);
-	const riseLog = !r.rise ? 'undefined' : ToLocalISOString(r.rise).substring(11, 19);
-	const setLog = !r.set ? 'undefined' : ToLocalISOString(r.set).substring(11, 19);
-	return `{when: ${whenLog}, rise: ${riseLog}, set: ${setLog}}`;
-}
+// function formatDebug(r: MoonDebug) : string {
+// 	const whenLog = !r.when ? 'undefined' : ToLocalISOString(r.when).substring(0, 10);
+// 	const riseLog = !r.rise ? 'undefined' : ToLocalISOString(r.rise).substring(11, 19);
+// 	const setLog = !r.set ? 'undefined' : ToLocalISOString(r.set).substring(11, 19);
+// 	return `{when: ${whenLog}, rise: ${riseLog}, set: ${setLog}}`;
+// }
